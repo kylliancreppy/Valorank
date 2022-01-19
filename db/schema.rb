72069e76d1bebdea9bd6fca1e9d2233535a71558
Kyllian Creppy <kyllian.creppy@outlook.fr>
2022-01-19 14:34:33 +0100

    Abilities model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_19_132500) do
+ActiveRecord::Schema.define(version: 2022_01_19_133342) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "abilities", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
